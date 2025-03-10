b9a1a20652e854f2300cf73eccb2277a939e6ee7
rosniz <zidanerosni80@gmail.com>
2025-03-10 03:47:29 +0100

    Mis A jour du logo et deplacement du restructuration du dossier des fichiers satic

--- recruitment_system/recruitment_system/urls.py
+++ recruitment_system/recruitment_system/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('', include('accounts.urls')),
     path('jobs/', include('jobs.urls')),
 ]
-
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
